44bc070adc5017d13fbb37ec572f96ed5b52cb77
Torqueware <torqueware@gmail.com>
2014-10-26 11:26:37 -0700

    added the sieve from problem3, but I suspect I will have to adapt it to incrimentally generate primes (rather than generate primes up to a certain number)

---
+++ problem7.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.3
+
+import math
+
+big    = 600851475143
+limit =
+prime = list()
+prime = [False] * (limit + 1)
+
+for x in range(1, int(math.sqrt(limit)) + 1):
+    for y in range(1, int(math.sqrt(limit)) + 1):
+        n = 4 * x**2 + y**2
+
+        if n <= limit and (n%12 == 1 or n%12 == 5):
+            prime[n] = not prime[n]
+
+        n = 3 * x**2 + y**2
+        if n <= limit and n%12 == 7:
+            prime[n] = not prime[n]
+
+        n = 3 * x**2 - y**2
+        if x > y and n <= limit and n%12 == 11:
+            prime[n] = not prime[n]
+
+for n in range(5, int(math.sqrt(limit))):
+    if prime[n]:
+        for k in range(n**2, limit + 1, n**2):
+            prime[k] = False
