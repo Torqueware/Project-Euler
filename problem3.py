9c83dcc2a513f4b1e5b5118609d12f23d23c67bc
Torqueware <torqueware@gmail.com>
2014-10-26 11:11:26 -0700

    Finally solved problem 3, did it by using another persons solution for calculating prime numbers. I used another implementation of the sieve of atkin. I ran into memory problems, so I only calculated the range from 1 to the square root of 600 trillion

--- problem3.py
+++ problem3.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3.3
-big = 600851475143
-limit = big
+
+import math
+
+big    = 600851475143
+limit  = int(math.sqrt(big))
+prime = list()
+prime = [False] * (limit + 1)
+
 candidates = []
 not_prime = []
 
-def eratosthenes2(n):
-    multiples = set()
-    for i in range(2, n+1):
-        if i not in multiples:
-            print(i)
-            #candidates.append(i)
-            multiples.update(range(i*i, n+1, i))
+for x in range(1, int(math.sqrt(limit)) + 1):
+    for y in range(1, int(math.sqrt(limit)) + 1):
+        n = 4 * x**2 + y**2
 
-eratosthenes2(big)
+        if n <= limit and (n%12 == 1 or n%12 == 5):
+            prime[n] = not prime[n]
+
+        n = 3 * x**2 + y**2
+        if n <= limit and n%12 == 7:
+            prime[n] = not prime[n]
+
+        n = 3 * x**2 - y**2
+        if x > y and n <= limit and n%12 == 11:
+            prime[n] = not prime[n]
+
+for n in range(5, int(math.sqrt(limit))):
+    if prime[n]:
+        for k in range(n**2, limit + 1, n**2):
+            prime[k] = False
+
+for n in range(5, limit):
+    if prime[n] and big%n == 0:
+        print(n)
