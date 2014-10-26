a7766ffeea1cd2191ab8df1d6b863a4cc83c3dae
Torqueware <torqueware@gmail.com>
2014-10-26 09:01:16 -0700

    I asked my father this question, and he gave a more efficient algorithm. I revised my answer - but the credit goes to Bill Millard.

--- problem1.py
+++ problem1.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3.3
+
 ans = 0
-threes = []
 
 for i in range(1, 1000):
-    if i%3 == 0 and i%5 == 0:
-        threes.append(i)
-    elif i%3 == 0:
-        threes.append(i)
-    elif i%5 == 0:
-        threes.append(i)
+    if i%3 == 0 or i%5 == 0:
+        ans += i
 
-print(sum(threes))
+print(ans)
