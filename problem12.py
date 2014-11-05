13480c4a9dd3c2893bff525b48a22d83730681f6
Torqueware <torqueware@gmail.com>
2014-11-05 08:34:03 -0800

    after a lot of careful thought on the n/m being a factor if n%m == 0 and m > n/m, I got problem 12 correct!

--- problem12.py
+++ problem12.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3.3
 
-triangles = [1]
-i = 2
+triangles = [1, 3, 6, 10, 15, 21, 28]
+i = 8
 
 while True:
     triangles.append(triangles[i - 2] + i)
-    divisors = 0
+    divisors = 2
 
-    for d in range(triangles[i - 2], 1, -1):
+    for d in range(2, triangles[i - 2] - 1):
+        f = triangles[i - 2]/d
+        if d > f:
+            break
         if triangles[i - 2]%d == 0:
             divisors += 1
+            if triangles[i - 2]%f == 0:
+                divisors += 1
 
     print("number ", triangles[i - 2], ". divisors ", divisors)
 
